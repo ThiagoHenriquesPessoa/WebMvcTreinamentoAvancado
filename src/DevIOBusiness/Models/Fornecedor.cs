625d96cec01778c4972c59a75532aead3015b33e
Thiago Henriques <thiagokxl5@gmail.com>
2021-11-21 11:50:26 -0300

    Criação das models e finalização da criação do Setup

---
+++ src/DevIOBusiness/Models/Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcTreinamentoBasico.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
---
+++ src/DevIOBusiness/Models/TipoFornecedor.cs
@@ -0,0 +1,8 @@
+﻿namespace AppMvcTreinamentoBasico.Models
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
